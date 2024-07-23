b6f1fbf702286c3f40806c7fca1978c02fc6cf23
nikoscharits24 <nikoscharits@yahoo.com>
2024-07-23 22:32:33 +0300

    Computer choice and Human choice established

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(array){
+    const randomChoice = Math.floor(Math.random() * array.length);
+    return array[randomChoice];
+}
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+
+function getHumanChoice(){
+    const humanChoice = prompt("Rock, Paper, Scissors", "");
+    return humanChoice;
+}
+
+
+
+
+console.log(getHumanChoice());
+console.log(getComputerChoice(choices));
